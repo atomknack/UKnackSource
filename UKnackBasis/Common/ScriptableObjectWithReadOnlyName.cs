b3fad0f232d187fecce9e3e005257ba209547c3e
atomknack <110350925+atomknack@users.noreply.github.com>
2023-06-22 17:25:11 +0400

    added ICommands

--- UKnackBasis/Common/ScriptableObjectWithReadOnlyName.cs
+++ UKnackBasis/Common/ScriptableObjectWithReadOnlyName.cs
@@ -4,7 +4,7 @@ namespace UKnack.Common;
 
 public class ScriptableObjectWithReadOnlyName : ScriptableObject
 {
-    // to protect scriptable object from errorous asset name change
+    // to protect scriptable object from errorous asset name change from accidental UnityEvent pick
     public new string name
     {
         //at current version get only sufficiently hides name from edit, if in future versions of UnityEditor it not enough, you can try to uncomment setter.

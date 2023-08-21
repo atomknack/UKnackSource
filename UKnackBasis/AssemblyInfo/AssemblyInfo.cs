26ed26e6d7fd10796be439b1f63cd712524074ce
atomknack <110350925+atomknack@users.noreply.github.com>
2023-08-22 00:03:04 +0400

    internals visible to magiclanjello

--- UKnackBasis/AssemblyInfo/AssemblyInfo.cs
+++ UKnackBasis/AssemblyInfo/AssemblyInfo.cs
@@ -7,4 +7,5 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UserAssemblyFriendOfUKnackCsFiles")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UserAssemblyFriendOfUKnackConcrete")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UKnackTests")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("magiclanjello")]
 
--- UKnackBasisConcrete/AssemblyInfo/AssemblyInfo.cs
+++ UKnackBasisConcrete/AssemblyInfo/AssemblyInfo.cs
@@ -6,4 +6,5 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UserAssemblyFriendOfUKnackCsFiles")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UserAssemblyFriendOfUKnackConcrete")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("UKnackTests")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("magiclanjello")]
 
